34760a5de69f9ff50e789f99de4ff885843a7210
Mazuh <marcell-mz@hotmail.com>
2016-04-26 16:46:52 -0300

    Interfacezinha rascunho pra equipe

--- src/teste_stricto_sensu_equipe.php
+++ src/teste_stricto_sensu_equipe.php
@@ -14,10 +14,36 @@ $equipe = $ppgp->equipe();
 
 ?>
 
+<style type="text/css">
+    #tabela-equipe{
+        text-align: center;
+    }
+    
+    #tabela-equipe tr{
+        /*border-bottom: 1px solid black;*/
+    }
+    
+    #tabela-equipe th{
+        background-color: #ecf0f1;
+    }
+    
+    #tabela-equipe td{
+        padding-left: 13px;
+        padding-right: 13px;
+        padding-bottom: 7px;
+    }
+    
+    #tabela-equipe .nome-professor{
+        text-align: left;
+    }
+    
+    
+</style>
+
 <h3>PPGP: Equipe</h3>
 <hr/>
 
-<table style="text-align: center">
+<table id="tabela-equipe">
     
     <tr>
         <th>Nome</th>
@@ -29,7 +55,7 @@ $equipe = $ppgp->equipe();
     <?php foreach ($equipe as $membro): ?>
     
     <tr id="<?php echo $membro['idEquipePrograma']; ?>">
-        <td style="text-align: left">
+        <td class="nome-professor">
             <?php echo $membro['nome']; ?>
             <?php if ($lattes=$ppgp->lattesDoDocentePorNome($membro['nome'])): ?>
             <a href="<?php echo $lattes; ?>" target="_blank"><img src="http://www.ufpa.br/ppba/images/lattes.gif" alt="CV Lattes"/></a><?php endif; ?>
