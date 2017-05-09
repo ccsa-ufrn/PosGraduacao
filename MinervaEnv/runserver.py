634cd40a555556bb1359230ec007c595f8ea2620
Mazuh <marcell-mz@hotmail.com>
2017-05-09 07:00:49 -0300

    Update server script for debugging and local dating

--- MinervaEnv/runserver.py
+++ MinervaEnv/runserver.py
@@ -1,15 +1,22 @@
 """
 This script runs the Minerva application using a development server.
 """
+import locale
 
 from os import environ
 from Minerva import app
 
 if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') # caution, this may crash in some OS
+
+    app.jinja_env.cache = {}
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
     HOST = environ.get('SERVER_HOST', 'localhost')
-    #HOST = '0.0.0.0'
+
     try:
         PORT = int(environ.get('SERVER_PORT', '4444'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    app.run(HOST, PORT, debug=True)
