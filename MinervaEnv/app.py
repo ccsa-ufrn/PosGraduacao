bb04cb9f3b87fd82578efde2698378a3a36c3bd4
Marcell Guilherme Costa da Silva <Mazuh@users.noreply.github.com>
2017-05-24 12:21:34 -0300

    Update app.py

--- MinervaEnv/app.py
+++ MinervaEnv/app.py
@@ -1,12 +1,12 @@
 """
 This script runs the Minerva application using a development server.
 """
-#import locale
+import locale
 
 from Minerva import app
 
 if __name__ == '__main__':
-    #locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') # caution, this may crash in some OS
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
     #app.jinja_env.cache = {}
     #app.jinja_env.auto_reload = True
@@ -18,4 +18,5 @@ if __name__ == '__main__':
     try:
         app.run(HOST, PORT)
     except PermissionError:
+        app.run('DEVELOPMENT MODE: running at localhost only!!!')
         app.run('localhost', 4444, debug=True)
